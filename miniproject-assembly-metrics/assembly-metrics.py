4a56cd45b4083937a15ca17310ee70d9627ed83e
Jing Gu <jgu41@jh.edu>
2025-09-04 16:56:47 -0400

    update to include the n50 calculation

--- miniproject-assembly-metrics/assembly-metrics.py
+++ miniproject-assembly-metrics/assembly-metrics.py
@@ -7,12 +7,22 @@ my_file = open(sys.argv[1])
 contigs =fasta.FASTAReader(my_file)
 total_length=0
 number_of_contigs=0
+contigs_lengths=[]
+cumulative_length=0
 
 for ident, sequence in contigs:
     #print(ident)
     length= len(sequence)
+    contigs_lengths.append(length)
     total_length += length
     number_of_contigs +=1
+
+contigs_lengths.sort(reverse=True)
+for i in range(len(contigs_lengths)):
+    cumulative_length += contigs_lengths[i]
+    if cumulative_length > total_length/2 :
+        print (f"sequence length of the shortest contig at 50% of the total assembly length is {contigs_lengths[i]}")
+        break 
 
 average_length= total_length/number_of_contigs
 print((f"Number of contigs:{number_of_contigs},\tTotal length:{total_length},\tAverage length:{average_length}"))
