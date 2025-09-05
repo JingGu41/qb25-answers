a2d68de902b807d01b762720c7b54e2d549c9679
Jing Gu <jgu41@jh.edu>
2025-09-05 00:30:06 -0400

    add functions for translation and abundance catalog

--- miniproject-codon-usage/mini-project-codon-usage.py
+++ miniproject-codon-usage/mini-project-codon-usage.py
@@ -2,12 +2,14 @@
 
 import sys
 import fasta
+import codons
 
 my_file = open(sys.argv[1])
 contigs =fasta.FASTAReader(my_file)
+aas={}
 
 for ident, sequence in contigs:
-    codon_list=[]
+    #codon_list=[]
     #print(ident)
     #print(sequence)
     #print(len(sequence))
@@ -17,8 +19,16 @@ for ident, sequence in contigs:
         #print(i)
         if i in counter:
             codon= sequence[i:i+3]
+            aa = (codons.forward).get(codon)
             #print(codon)
-            codon_list.append(codon)
+            #print(aa)
+            #codon_list.append(codon)
+            if aa not in aas:
+                aas[aa]=1
+            else:
+                aas[aa] += 1 
         else:
             continue 
     #print(len(codon_list))
+
+print(aas)
